56fcaed25fc49db5c1270e35fc6671f9eb716936
SoundSlab <administrator@Mac-mini.local>
2020-09-28 14:12:22 -0400

    initial

---
+++ HashMaps_drills.js
@@ -0,0 +1,42 @@
+const HashMap = require('./HashMap');
+
+function main() {
+
+    lotrFunc = () => {
+        let lotr = new HashMap();
+        lotr.MAX_LOAD_RATIO = 0.5;
+        lotr.SIZE_RATIO = 3;
+        lotr.set('Hobbit', 'Bilbo');
+        lotr.set('Hobbit', 'Frodo');
+        lotr.set('Wizard', 'Gandalf');
+        lotr.set('Human', 'Aragorn');
+        lotr.set('Elf', 'Legolas');
+        lotr.set('Maiar', 'Sauron');
+        lotr.set('Maiar', 'The Necromancer');
+        lotr.set('RingBearer', 'Gollum');
+        lotr.set('LadyOfLoght', 'Galadriel');
+        lotr.set('HalfElven', 'Arwen');
+        lotr.set('Ent', 'Treebeard');
+        console.log(lotr.get('HalfElvenc'))
+    };
+
+    //This function sets and resets the key 'Hello World' on both hash maps.
+    WhatDoesThisDo = () => {
+        let str1 = 'Hello World.';
+        let str2 = 'Hello World.';
+        let map1 = new HashMap();
+        map1.set(str1,10);
+        map1.set(str2,20);
+        let map2 = new HashMap();
+        let str3 = str1;
+        let str4 = str2;
+        map2.set(str3,20);
+        map2.set(str4,10);
+    
+        console.log(map1.get(str1));
+        console.log(map2.get(str3));
+    };
+
+}
+
+main();
